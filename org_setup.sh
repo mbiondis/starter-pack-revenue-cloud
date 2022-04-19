cbe3963f9acd037ea94664e7be22701b3bdd0632
Martin <martinbiondi92@gmail.com>
2022-04-19 10:44:23 +0100

    Updated setup files

--- org_setup.sh
+++ org_setup.sh
@@ -5,12 +5,13 @@ orgAlias=$1
 
 echo " "
 echo "******* Pushing source code..."
-#sfdx force:source:push -u "${orgAlias}" -f
+sfdx force:source:push -u "${orgAlias}" -f
 
 echo " "
 echo "******* Assigning permisission sets..."
 
 # Managed Package
+#sfdx force:user:create -f config/admin-user-def.json -u "${orgAlias}"
 sfdx force:user:permsetlicense:assign -n "Salesforce CPQ License" -u "${orgAlias}"
 sfdx force:user:permsetlicense:assign -n "Salesforce CPQ AA License" -u "${orgAlias}"
 sfdx force:user:permset:assign -n "SteelBrickCPQAdmin" -u "${orgAlias}"
@@ -19,6 +20,9 @@ sfdx force:user:permset:assign -n "AdvancedApprovalsAdmin" -u "${orgAlias}"
 
 # Starter Pack
 sfdx force:user:permset:assign -n "RevenueCloud_Admin" -u "${orgAlias}"
+
+# Sample Data
+sfdx sfdmu:run -s csvfile -p data/csv -u "${orgAlias}"
 
 echo " "
 echo "******* Opening the environment..."
